f71ed0b5c044467826724889b9fe583c5b56d57f
Illya Moskvin <imoskvin@artic.edu>
2021-03-04 13:52:16 -0600

    Add file counts to compare.sh [WEB-1983]

--- compare.sh
+++ compare.sh
@@ -8,10 +8,12 @@ DIR_IMAGES_RECOMPRESSED="$DIR_IMAGES/recompressed"
 for DIR_SIZE in $(find "$DIR_IMAGES_DOWNLOADED" -type d -mindepth 1 -exec basename {} \; | sort -n); do
     DIR_OLD="$DIR_IMAGES_DOWNLOADED/$DIR_SIZE"
     DIR_NEW="$DIR_IMAGES_RECOMPRESSED/$DIR_SIZE"
+    COUNT_OLD="$(ls -1 "$DIR_OLD" | wc -l | awk '{print $1}')"
+    COUNT_NEW="$(ls -1 "$DIR_NEW" | wc -l | awk '{print $1}')"
     SIZE_OLD_DISPLAY="$(du -sh "$DIR_OLD" | awk '{print $1}')"
     SIZE_NEW_DISPLAY="$(du -sh "$DIR_NEW" | awk '{print $1}')"
     SIZE_OLD="$(du -s "$DIR_OLD" | awk '{print $1}')"
     SIZE_NEW="$(du -s "$DIR_NEW" | awk '{print $1}')"
     SIZE_PERCENT="$(printf "%0.2f\n" $(echo "$SIZE_NEW/$SIZE_OLD*100" | bc -l))% of original"
-    echo "$DIR_SIZE: From $SIZE_OLD_DISPLAY to $SIZE_NEW_DISPLAY ($SIZE_PERCENT)"
+    echo "$DIR_SIZE: From $SIZE_OLD_DISPLAY to $SIZE_NEW_DISPLAY ($SIZE_PERCENT) (File count: $COUNT_OLD, $COUNT_NEW)"
 done
