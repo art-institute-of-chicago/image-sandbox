43933bb0fc9b49abdaaeef3648e01f3fc5257a19
Illya Moskvin <imoskvin@artic.edu>
2021-03-04 13:11:22 -0600

    Make compare.sh sort dirs numerically [WEB-1983]

--- README.md
+++ README.md
@@ -22,9 +22,9 @@ This will give you a result like so:
 
 ```
 $ ./compare.sh
-843: From 32M to 11M (33.76% of original)
-400: From 8.5M to 2.7M (32.11% of original)
 200: From 2.2M to 896K (39.16% of original)
+400: From 8.5M to 2.7M (32.11% of original)
 600: From 16M to 5.7M (34.94% of original)
+843: From 32M to 11M (34.02% of original)
 1686: From 52M to 13M (24.30% of original)
 ```
--- compare.sh
+++ compare.sh
@@ -5,13 +5,13 @@ DIR_IMAGES="$DIR_SCRIPT/images"
 DIR_IMAGES_DOWNLOADED="$DIR_IMAGES/downloaded"
 DIR_IMAGES_RECOMPRESSED="$DIR_IMAGES/recompressed"
 
-for DIR_OLD in $(find "$DIR_IMAGES_DOWNLOADED" -type d -mindepth 1); do
-    CURRENT_SIZE="$(basename "$DIR_OLD")"
-    DIR_NEW="$DIR_IMAGES_RECOMPRESSED/$CURRENT_SIZE"
+for DIR_SIZE in $(find "$DIR_IMAGES_DOWNLOADED" -type d -mindepth 1 -exec basename {} \; | sort -n); do
+    DIR_OLD="$DIR_IMAGES_DOWNLOADED/$DIR_SIZE"
+    DIR_NEW="$DIR_IMAGES_RECOMPRESSED/$DIR_SIZE"
     SIZE_OLD_DISPLAY="$(du -sh "$DIR_OLD" | awk '{print $1}')"
     SIZE_NEW_DISPLAY="$(du -sh "$DIR_NEW" | awk '{print $1}')"
     SIZE_OLD="$(du -s "$DIR_OLD" | awk '{print $1}')"
     SIZE_NEW="$(du -s "$DIR_NEW" | awk '{print $1}')"
     SIZE_PERCENT="$(printf "%0.2f\n" $(echo "$SIZE_NEW/$SIZE_OLD*100" | bc -l))% of original"
-    echo "$CURRENT_SIZE: From $SIZE_OLD_DISPLAY to $SIZE_NEW_DISPLAY ($SIZE_PERCENT)"
+    echo "$DIR_SIZE: From $SIZE_OLD_DISPLAY to $SIZE_NEW_DISPLAY ($SIZE_PERCENT)"
 done
