4eab1d46db033d9080e848a66e40625897c5975a
Illya Moskvin <imoskvin@artic.edu>
2021-03-17 16:09:43 -0500

    Show average image file size [WEB-1983]

--- compare.sh
+++ compare.sh
@@ -24,8 +24,11 @@ for DIR_SIZE in $(find "$DIR_IMAGES_A" -type d -mindepth 1 -exec basename {} \; 
     COUNT_NEW="$(ls -1 "$DIR_NEW" | wc -l | awk '{print $1}')"
     SIZE_OLD_DISPLAY="$(du -sh "$DIR_OLD" | awk '{print $1}')"
     SIZE_NEW_DISPLAY="$(du -sh "$DIR_NEW" | awk '{print $1}')"
-    SIZE_OLD="$(du -s "$DIR_OLD" | awk '{print $1}')"
-    SIZE_NEW="$(du -s "$DIR_NEW" | awk '{print $1}')"
+    # macOS du lacks -b (https://superuser.com/a/402000)
+    SIZE_OLD="$(du -sk "$DIR_OLD" | awk '{print $1}')"
+    SIZE_NEW="$(du -sk "$DIR_NEW" | awk '{print $1}')"
+    AVG_SIZE_OLD_DISPLAY="$(printf "%.0f\n" $(echo "$SIZE_OLD/$COUNT_OLD*1024" | bc -l) | numfmt --to iec --format "%.1f")"
+    AVG_SIZE_NEW_DISPLAY="$(printf "%.0f\n" $(echo "$SIZE_NEW/$COUNT_NEW*1024" | bc -l) | numfmt --to iec --format "%.1f")"
     SIZE_PERCENT="$(printf "%0.2f\n" $(echo "$SIZE_NEW/$SIZE_OLD*100" | bc -l))% of original"
-    echo "$DIR_SIZE: From $SIZE_OLD_DISPLAY to $SIZE_NEW_DISPLAY ($SIZE_PERCENT) (File count: $COUNT_OLD, $COUNT_NEW)"
+    echo "$DIR_SIZE: From $SIZE_OLD_DISPLAY to $SIZE_NEW_DISPLAY ($SIZE_PERCENT) (File count: $COUNT_OLD, $COUNT_NEW) (Avg Size/File: $AVG_SIZE_OLD_DISPLAY to $AVG_SIZE_NEW_DISPLAY)"
 done
