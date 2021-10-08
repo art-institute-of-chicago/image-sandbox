3ccc0ab91161e2154ef94d69b2827a812cd8f7ab
Nadine Siddharta <nadinea.siddharta@gmail.com>
2021-10-08 15:33:09 -0500

    Fix issues in download-testcases.sh [IMG-41]

--- matching/download-testcases.sh
+++ matching/download-testcases.sh
@@ -8,7 +8,6 @@ fi
 DIR_SCRIPT="$(dirname "${BASH_SOURCE[0]}")"
 DIR_IMAGES="$DIR_SCRIPT/images"
 DIR_IMAGES_DOWNLOADED="$DIR_IMAGES/downloaded"
-
 FILE_RESPONSE='/tmp/aic-images-download.json'
 API_URL='https://api.artic.edu/api/v1/artworks?ids=14561,137125,109819,157156,61603,2189,186049,129884,27992,16568,16571,8991,117271,160201,185222,27,2582,24645,14620,15897,20684,111628,76244,34181,117319,73903,61128,111060,100472,100829&fields=id,title,image_id'
 API_QUERY='{
@@ -33,10 +32,6 @@ API_COUNT="$(echo "$API_RESPONSE" | jq -r '.data | length')"
 
 IMAGE_SIZE=843
 
-if [ ! -d "$DIR_IMAGES_DOWNLOADED/$IMAGE_SIZE" ]; then
-   mkdir "$DIR_IMAGES_DOWNLOADED/$IMAGE_SIZE"
-fi
-
 for API_INDEX in $(seq $API_COUNT); do
     IMAGE_ID="$(echo "$API_RESPONSE" | jq -r ".data[$API_INDEX].image_id")"
 
@@ -46,7 +41,7 @@ for API_INDEX in $(seq $API_COUNT); do
 
     IS_PUBLIC_DOMAIN="$(echo "$API_RESPONSE" | jq -r ".data[$API_INDEX].is_public_domain")"
 
-        FILE_IMAGE="$DIR_IMAGES_DOWNLOADED/$IMAGE_SIZE/$IMAGE_ID.jpg"
+        FILE_IMAGE="$DIR_IMAGES_DOWNLOADED/$IMAGE_ID.jpg"
 
         if [ "$IMAGE_SIZE" -lt "844" ] || [ "$IS_PUBLIC_DOMAIN" == 'true' ]; then
             echo "$FILE_IMAGE downloading"
