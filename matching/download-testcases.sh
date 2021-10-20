34f358f8786ec94d609c9d7dfcd340e995041c74
Nadine Siddharta <nadinea.siddharta@gmail.com>
2021-10-20 10:07:06 -0500

    Fix comments on download-testcases.sh script [IMG-41]

--- matching/download-testcases.sh
+++ matching/download-testcases.sh
@@ -9,9 +9,21 @@ DIR_SCRIPT="$(dirname "${BASH_SOURCE[0]}")"
 DIR_IMAGES="$DIR_SCRIPT/images"
 DIR_IMAGES_DOWNLOADED="$DIR_IMAGES/downloaded"
 FILE_RESPONSE='/tmp/aic-images-download.json'
-API_URL='https://api.artic.edu/api/v1/artworks?ids=14561,137125,109819,157156,61603,2189,186049,129884,27992,16568,16571,8991,117271,160201,185222,27,2582,24645,14620,15897,20684,111628,76244,34181,117319,73903,61128,111060,100472,100829&fields=id,title,image_id'
+API_URL='https://api.artic.edu/api/v1/artworks'
 API_QUERY='{
     "resources": "artworks",
+    "ids": [
+        14561,137125,109819,
+        157156,61603,2189,
+        186049,129884,27992,
+        16568,16571,8991,
+        117271,160201,185222,
+        27,2582,24645,
+        14620,15897,20684,
+        111628,76244,34181,
+        117319,73903,61128,
+        111060,100472,100829
+    ],
     "fields": [
         "id",
         "title",
@@ -41,25 +53,25 @@ for API_INDEX in $(seq $API_COUNT); do
 
     IS_PUBLIC_DOMAIN="$(echo "$API_RESPONSE" | jq -r ".data[$API_INDEX].is_public_domain")"
 
-        FILE_IMAGE="$DIR_IMAGES_DOWNLOADED/$IMAGE_ID.jpg"
+    FILE_IMAGE="$DIR_IMAGES_DOWNLOADED/$IMAGE_ID.jpg"
 
-        if [ "$IMAGE_SIZE" -lt "844" ] || [ "$IS_PUBLIC_DOMAIN" == 'true' ]; then
+    if [ "$IMAGE_SIZE" -lt "844" ] || [ "$IS_PUBLIC_DOMAIN" == 'true' ]; then
             echo "$FILE_IMAGE downloading"
 
-            if [ "$IMAGE_SIZE" -eq "3000" ]; then
-                IMAGE_SIZE_URL='!3000,3000'
-            else
-                IMAGE_SIZE_URL="$IMAGE_SIZE,"
-            fi
-
-            STATUS="$(curl -s "https://lakeimagesweb.artic.edu/iiif/2/$IMAGE_ID/full/$IMAGE_SIZE_URL/0/default.jpg" -w %{http_code} -m 5 --output "$FILE_IMAGE")"
-
-            if [ ! "$STATUS" = "200" ]; then
-                echo "Sorry, we are having trouble downloading the image. Try again later!" >&2
-                rm "$FILE_IMAGE"
-                exit 1
-            fi
+        if [ "$IMAGE_SIZE" -eq "3000" ]; then
+            IMAGE_SIZE_URL='!3000,3000'
+    	else
+            IMAGE_SIZE_URL="$IMAGE_SIZE,"
         fi
+
+        STATUS="$(curl -s "https://lakeimagesweb.artic.edu/iiif/2/$IMAGE_ID/full/$IMAGE_SIZE_URL/0/default.jpg" -w %{http_code} -m 5 --output "$FILE_IMAGE")"
+
+        if [ ! "$STATUS" = "200" ]; then
+            echo "Sorry, we are having trouble downloading the image. Try again later!" >&2
+            rm "$FILE_IMAGE"
+            exit 1
+        fi
+    fi
 done
 
 # Clean up temporary files
