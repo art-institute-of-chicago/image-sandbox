600f7382c86bcfe5c616a12d12405fa32f0323cb
Nadine Siddharta <nadinea.siddharta@gmail.com>
2021-11-01 09:12:50 -0500

    Fix subdirectory issue when transforming images [IMG-40]

--- matching/convert-images.sh
+++ matching/convert-images.sh
@@ -25,31 +25,31 @@ done
 
 IMAGE_RESIZES=("50%" "25%" "150%" "200%")
 for IMAGE_RESIZE in "${IMAGE_RESIZES[@]}"; do 
-    cp -r "$DIR_IMAGES_DOWNLOADED" "$DIR_IMAGES/scale-proportional/$IMAGE_RESIZE"
+    mkdir "$DIR_IMAGES/scale-proportional/$IMAGE_RESIZE" 
     mogrify -resize $IMAGE_RESIZE -path "$DIR_IMAGES/scale-proportional/$IMAGE_RESIZE" "$DIR_IMAGES_DOWNLOADED/"*.jpg
 done
 
 IMAGE_ROTATES=("90" "180" "270" "23" "45" "67" "156")
 for IMAGE_ROTATE in "${IMAGE_ROTATES[@]}"; do
-    cp -r "$DIR_IMAGES_DOWNLOADED" "$DIR_IMAGES/rotate/$IMAGE_ROTATE"
+    mkdir "$DIR_IMAGES/rotate/$IMAGE_ROTATE"
     mogrify -rotate -$IMAGE_ROTATE -path "$DIR_IMAGES/rotate/$IMAGE_ROTATE" "$DIR_IMAGES_DOWNLOADED/"*.jpg
 done
 
 IMAGE_COLORS=("-negate" "-colorspace Gray" "+level-colors red,yellow" "+level-colors yellow,blue" "+level-colors blue,red" "+level 20%" "-level 20%")
 for IMAGE_COLOR in "${IMAGE_COLORS[@]}"; do
-    cp -r "$DIR_IMAGES_DOWNLOADED" "$DIR_IMAGES/color-shift/$IMAGE_COLOR"
+    mkdir "$DIR_IMAGES/color-shift/$IMAGE_COLOR"
     mogrify $IMAGE_COLOR -path "$DIR_IMAGES/color-shift/$IMAGE_COLOR" "$DIR_IMAGES_DOWNLOADED/"*.jpg
 done
 
 IMAGE_CROPS=("NorthWest" "NorthEast" "Center" "SouthWest" "SouthEast")
 for IMAGE_CROP in "${IMAGE_CROPS[@]}"; do
-    cp -r "$DIR_IMAGES_DOWNLOADED" "$DIR_IMAGES/crop/$IMAGE_CROP"
+    mkdir "$DIR_IMAGES/crop/$IMAGE_CROP"
     mogrify -gravity $IMAGE_CROP -crop 50%x+0+0 -path "$DIR_IMAGES/crop/$IMAGE_CROP" "$DIR_IMAGES_DOWNLOADED/"*.jpg
 done
 
 IMAGE_SCALE_DISPS=("500x843" "250x843" "100x843" "843x500" "843x250" "843x100")
-for IMAGE_SCALE_DISP in "${IMAGE_SCALE_DISPS[@]}"; do 
-    cp -r "$DIR_IMAGES_DOWNLOADED" "$DIR_IMAGES/scale-disproportional/$IMAGE_SCALE_DISP"
+for IMAGE_SCALE_DISP in "${IMAGE_SCALE_DISPS[@]}"; do
+    mkdir "$DIR_IMAGES/scale-disproportional/$IMAGE_SCALE_DISP" 
     mogrify -resize $IMAGE_SCALE_DISP! -path "$DIR_IMAGES/scale-disproportional/$IMAGE_SCALE_DISP" "$DIR_IMAGES_DOWNLOADED/"*.jpg
 done
 
