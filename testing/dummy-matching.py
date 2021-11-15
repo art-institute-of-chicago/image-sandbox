0ab59dc3b26d2f9d6f48fab07f4da8865047265d
Nadine Siddharta <nadinea.siddharta@gmail.com>
2021-11-15 16:53:33 -0600

    Move dummy matching script into seperate directory [IMG-46]

--- testing/dummy-matching.py
+++
@@ -1,8 +0,0 @@
-import os 
-import random
-
-def match_image(file_path):
-    downloaded_path = "../matching/images/downloaded"
-    image_file = random.choice(os.listdir(downloaded_path))
-    return image_file.split(".",1)[0]
-    
