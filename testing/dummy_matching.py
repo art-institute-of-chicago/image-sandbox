18620c7ace340796c26967d7f3be3a334796ea51
Nadine Siddharta <nadinea.siddharta@gmail.com>
2021-11-28 21:31:03 -0600

    Add PEP 8 compliance to dummymatching.py [IMG-46]

--- testing/dummy_matching.py
+++ testing/dummy_matching.py
@@ -1,7 +1,9 @@
-import os 
+import os
 import random
+
+
 class dummy_matching():
     def match_image(file_path):
         downloaded_path = "../matching/images/downloaded"
         image_file = random.choice(os.listdir(downloaded_path))
-        return image_file.split(".",1)[0]
+        return image_file.split(".", 1)[0]
