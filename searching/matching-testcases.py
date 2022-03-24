c8b0187087058325da9547bcfa6199d39da2d70f
Nadine Siddharta <nadinea.siddharta@gmail.com>
2022-03-24 08:28:09 -0500

    Create script to query image hashes [IMG-57]

---
+++ searching/matching-testcases.py
@@ -0,0 +1,140 @@
+import unittest
+import search_hashes
+import os
+from unittest import TestCase
+import random
+import argparse
+match = search_hashes.search_hash
+NUM_TESTS = 2
+
+
+class QuickTestSuite(TestCase):
+    def test_output(self):
+        file = ("../matching/images/crop/center/"
+                "0f1cc0e0-e42e-be16-3f71-2022da38cb93.jpg")
+        result = match.match_image(file)
+        self.assertTrue(len(result) > 0)
+
+    def test_match(self):
+        file = ("../matching/images/crop/center/"
+                "0f1cc0e0-e42e-be16-3f71-2022da38cb93.jpg")
+        file_name = file.split('/')[-1]
+        result = match.match_image(file)
+        self.assertEqual(result, file_name)
+
+    def shared_quick_test(self, dir_path):
+        directory = os.listdir(dir_path)
+        for k in range(len(directory)):
+            current_dir = directory[k]
+            if current_dir != ".DS_Store":
+                folder_path = dir_path + "/" + str(current_dir)
+                images = [x for x in os.listdir(folder_path) if
+                          x.endswith(".jpg")]
+                for i in range(1, NUM_TESTS+1):
+                    current_image = random.choice(images)
+                    # current_image = current_image.split(".", 1)[0]
+                    image_path = folder_path + "/" + str(current_image)
+                    result = match.match_image(image_path)
+                    with self.subTest(i=("QuickTestSuite", NUM_TESTS*k+i)):
+                        self.assertEqual(result, current_image)
+
+    def test_rotate(self):
+        rotate_path = "../matching/images/rotate"
+        self.shared_quick_test(rotate_path)
+
+    def test_crop(self):
+        crop_path = "../matching/images/crop"
+        self.shared_quick_test(crop_path)
+
+    def test_color_shifted(self):
+        color_shift_path = "../matching/images/color-shift"
+        self.shared_quick_test(color_shift_path)
+
+    def test_scale_proportional(self):
+        scale_proportional_path = "../matching/images/scale-proportional"
+        self.shared_quick_test(scale_proportional_path)
+
+    def test_scale_disproportional(self):
+        scale_disproportional_path = "../matching/images/scale-disproportional"
+        self.shared_quick_test(scale_disproportional_path)
+
+    def test_change_exif(self):
+        change_exif_path = "../matching/images/change-exif"
+        directory = os.listdir(change_exif_path)
+        for i in range(1, NUM_TESTS+1):
+            current_image = random.choice(directory)
+            image_path = change_exif_path + "/" + str(current_image)
+            result = match.match_image(image_path)
+            with self.subTest(i=("QuickTestSuite", i)):
+                self.assertEqual(result, current_image)
+
+
+class FullTestSuite(TestCase):
+    def shared_full_test(self, dir_path):
+        directory = os.listdir(dir_path)
+        test_index = 0
+        for k in range(len(directory)):
+            current_dir = directory[k]
+            if current_dir != ".DS_Store":
+                folder_path = dir_path + "/" + str(current_dir)
+                images = [x for x in os.listdir(folder_path) if
+                          x.endswith(".jpg")]
+                for i in range(len(images)):
+                    current_image = images[i]
+                    # current_image = current_image.split(".", 1)[0]
+                    image_path = folder_path + "/" + str(current_image)
+                    result = match.match_image(image_path)
+                    with self.subTest(i=("FullTestSuite", test_index)):
+                        self.assertEqual(result, current_image)
+                    test_index += 1
+
+    def test_rotate(self):
+        rotate_path = "../matching/images/rotate"
+        self.shared_full_test(rotate_path)
+
+    def test_crop(self):
+        crop_path = "../matching/images/crop"
+        self.shared_full_test(crop_path)
+
+    def test_color_shifted(self):
+        color_shift_path = "../matching/images/color-shift"
+        self.shared_full_test(color_shift_path)
+
+    def test_scale_proportional(self):
+        scale_proportional_path = "../matching/images/scale-proportional"
+        self.shared_full_test(scale_proportional_path)
+
+    def test_scale_disproportional(self):
+        scale_disproportional_path = "../matching/images/scale-disproportional"
+        self.shared_full_test(scale_disproportional_path)
+
+    def test_change_exif(self):
+        change_exif_path = "../matching/images/change-exif"
+        directory = os.listdir(change_exif_path)
+        for i in range(len(directory)):
+            current_image = directory[i]
+            image_path = change_exif_path + "/" + str(current_image)
+            result = match.match_image(image_path)
+            with self.subTest(i=("FullTestSuite", i)):
+                self.assertEqual(result, current_image)
+
+
+def run_testsuite():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--full", help="run full test suite",
+                        action="store_true")
+    args = parser.parse_args()
+    if args.full:
+        test_class = FullTestSuite
+    else:
+        test_class = QuickTestSuite
+    loader = unittest.TestLoader()
+    runner = unittest.TextTestRunner()
+    current_suite = loader.loadTestsFromTestCase(test_class)
+    test_suite = unittest.TestSuite(current_suite)
+    results = runner.run(test_suite)
+    return results
+
+
+if __name__ == "__main__":
+    run_testsuite()
---
+++ searching/search_hashes.py
@@ -0,0 +1,49 @@
+import sys
+import image_hash
+import json
+from elasticsearch import Elasticsearch
+import datetime
+import re
+from PIL import Image
+import imagehash
+
+
+class search_hash():
+    def get_hash(file_path):
+        image_hash = imagehash.average_hash(Image.open(file_path))
+        return image_hash
+
+    def create_query(image_hash):
+        # https://github.com/JohannesBuchner/imagehash/issues/51
+        # https://stackoverflow.com/questions/61791924
+        curr_hash_flattened = image_hash.hash.flatten().tolist()
+        hash_fields = {}
+
+        hash_fields = []
+        for i in range(len(curr_hash_flattened)):
+            term = {}
+            term['term'] = {'hash_' + str(i): curr_hash_flattened[i]}
+            hash_fields.append(term)
+
+        query_dict = {
+                'query': {
+                    'bool': {
+                        'minimum_should_match': "80%",
+                        'should': hash_fields
+                    }
+                }
+        }
+        return query_dict
+
+    def match_image(file_path):
+        es = Elasticsearch("http://localhost:2200")
+        image_hash = search_hash.get_hash(file_path)
+        query = search_hash.create_query(image_hash)
+        res = es.search(index="hash_index", doc_type="image", body=query)
+        # print(res['hits']['hits'][0]['_source']['image_file_name'])
+        num_hits = res['hits']['total']
+        if (num_hits < 1):
+            return("no match")
+        else:
+            output_match = res['hits']['hits'][0]['_source']['image_file_name']
+            return output_match
